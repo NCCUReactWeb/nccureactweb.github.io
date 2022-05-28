80411058324c6129a356b7cfd4821a238483b964
augustinlu <augustinlu@cpht.pro>
2022-05-29 04:38:16 +0800

    for teaching purpose

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,19 +1,17 @@
 import App from "./App";
 import { screen, render } from "@testing-library/react";
 
-describe("test if App acts as a profile component", () => {
-  test("profile component should have a img component", () => {
-    render(<App />);
-    expect(screen.getByRole("img")).toBeTruthy();
-  });
+test("profile component should have a img component", () => {
+  render(<App />);
+  expect(screen.getByRole("img")).toBeTruthy();
+});
 
-  test("profile component should have a paragraph component to show info", () => {
-    render(<App />);
-    expect(screen.getByTestId("desc")).toBeTruthy();
-  });
+test("profile component should have a paragraph component to show info", () => {
+  render(<App />);
+  expect(screen.getByTestId("desc")).toBeTruthy();
+});
 
-  test("img component should have alt text", () => {
-    render(<App alt="this is react icon" />);
-    expect(screen.getByRole("img")).toHaveAttribute("alt");
-  });
+test("img component should have alt text", () => {
+  render(<App alt="this is react icon" />);
+  expect(screen.getByRole("img")).toHaveAttribute("alt");
 });
